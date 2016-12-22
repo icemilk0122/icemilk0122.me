45255278e4f81796e944f4afe4aff55f9ea5ad02
Wes Chuang <icemilk0122@gmail.com>
2016-12-23 04:15:03 +0800

    hammer

--- coffee/js/Main.js
+++ coffee/js/Main.js
@@ -26,7 +26,7 @@ window.onload = function() {
 
 function onPinch(ev)
 {
-	alert("pinch");
+	//alert("pinch");
 	alert(game.state.current);
 	//game.state.states[game.state.current].onPinch(ev.scale);
 }
