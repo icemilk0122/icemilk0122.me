ae2743ac22a7b448640790d151013f9db81359a5
Wes Chuang <icemilk0122@gmail.com>
2016-12-23 03:30:28 +0800

    add hammer

--- coffee/js/Main.js
+++ coffee/js/Main.js
@@ -2,16 +2,15 @@ window.onload = function() {
 	// Create your Phaser game and inject it into an auto-created canvas.
 	// We did it in a window.onload event, but you can do it anywhere (requireJS
 	// load, anonymous function, jQuery dom ready, - whatever floats your boat)
-	var mc = new Hammer.Manager(myElement);
+	var mc = new Hammer.Manager(document.getElementsByTagName("BODY")[0]);
 	var pinch = new Hammer.Pinch();
 	var rotate = new Hammer.Rotate();
 	// we want to detect both the same time
 	pinch.recognizeWith(rotate);
 	// add to the Manager
 	mc.add([pinch, rotate]);
-	mc.on("pinch rotate", function(ev) {
-	  trace(ev.type);
-});
+	mc.on("pinchstart pinchmove", onPinch);
+	mc.on("rotatestart rotatemove", onRotate);
 
 	var game = new Phaser.Game(1024, 768, Phaser.AUTO);
 
@@ -24,3 +23,14 @@ window.onload = function() {
 	// Now start the Boot state.
 	game.state.start("Boot");
 };
+
+function onPinch(ev)
+{
+	console.log(ev.scale);
+	alert(ev.scale);
+}
+
+function onRotate(ev)
+{
+	console.log(ev.rotation);
+}
