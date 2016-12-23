7c2e73f0b09b80f4cf4e2495fd07ec0fb00ea8fe
Wes Chuang <icemilk0122@gmail.com>
2016-12-23 10:11:36 +0800

    v1

--- coffee/js/Menu.js
+++ coffee/js/Menu.js
@@ -9,15 +9,18 @@ function Menu() {
 var proto = Object.create(Phaser.State);
 Menu.prototype = proto;
 
-Menu.prototype.preload = function() {
-	this.load.pack("start", "assets/assets-pack.json");
-};
-
 Menu.prototype.create = function() {
-	var sprite = this.add.sprite(this.world.centerX, this.world.centerY,
-			"tap-to-start");
-	sprite.anchor.set(0.5, 0.5);
-	this.input.onDown.add(this.startGame, this);
+	var indexbg = this.add.sprite(this.world.centerX, this.world.centerY,
+			"index");
+	indexbg.anchor.set(0.5, 0.5);
+	var start_btn = this.add.sprite(170, 670,
+			"start_btn");
+	start_btn.anchor.set(0.5, 0);
+	start_btn.inputEnabled = true;
+	start_btn.events.onInputDown.add(this.startGame);
+	var fb_btn = this.add.sprite(890, 650,
+			"fb_btn");
+	fb_btn.anchor.set(0.5, 0);
 };
 
 Menu.prototype.startGame = function() {
