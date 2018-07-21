dc601265d83e3da363b24cd1b753ff1a51790b4f
Wes Chuang <icemilk0122@gmail.com>
2018-07-21 17:55:22 +0800

    update wording

--- wellkanghome/dist/scripts/main.js
+++ wellkanghome/dist/scripts/main.js
@@ -30,25 +30,20 @@
 	});
 
 	// Header.
-	if ($banner.length > 0 && $header.hasClass('alt')) {
+	$header.removeClass('alt');
+	// if ($banner.length > 0
+	// &&	$header.hasClass('alt')) {
 
-		$window.on('resize', function () {
-			$window.trigger('scroll');
-		});
+	// 	$window.on('resize', function() { $window.trigger('scroll'); });
 
-		$banner.scrollex({
-			bottom: $header.outerHeight(),
-			terminate: function terminate() {
-				$header.removeClass('alt');
-			},
-			enter: function enter() {
-				$header.addClass('alt');
-			},
-			leave: function leave() {
-				$header.removeClass('alt');
-			}
-		});
-	}
+	// 	$banner.scrollex({
+	// 		bottom:		$header.outerHeight(),
+	// 		terminate:	function() { $header.removeClass('alt'); },
+	// 		enter:		function() { $header.addClass('alt'); },
+	// 		leave:		function() { $header.removeClass('alt'); }
+	// 	});
+
+	// }
 
 	// Menu.
 	var $menu = $('#menu');
